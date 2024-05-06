dd9e677d39a59b68a51cc33b44948daf057a6966
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-05-06 11:57:22 +0200

    Add validate data on entry

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  let result = "";
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  // Converts input to lower case
+  input = input.toLowerCase();
 
-            document.getElementById('result').innerText = result;
+  // Takes the first 4 characters and checks if its equal to "pet_"
+  let prefix = input.slice(0, 4);
+  prefix = prefix.includes("pet_");
+
+  // Takes the year converts it to a number then checks if its in range
+  let petBirthYear = input.slice(4, 8);
+
+  petBirthYear = +petBirthYear;
+
+  if (petBirthYear >= 1900 && petBirthYear <= 2024) {
+    petBirthYear = true;
+  } else {
+    petBirthYear = false;
+  }
+
+  // Checks to see if a name has been inputed
+  petName = input.slice(8);
+  if (petName.length > 0) {
+    petName = true;
+  } else {
+    petName = false;
+  }
+
+  // Checks if input is empty || starts with 'pet_' and followed by alphanumeric characters and name
+
+  if (input === "" || input == null) {
+    result = "Error Input Field Empty";
+  } else if (prefix === true && petBirthYear === true && petName === true) {
+    result = "Valid Syntax";
+  } else {
+    result = "Invalid Syntax";
+  }
+
+  document.getElementById("result").innerText = result;
 }
-
-
